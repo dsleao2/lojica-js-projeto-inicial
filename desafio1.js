9c179188315ecd0e8afd80c73ea8fec17c717799
Daniele Santana <dssleao2@icloud.com>
2025-03-28 23:20:23 -0300

    Desafio inicial
    Usamos o alert para exibir uma mensagem passando algumas instruções sobre o programa e usamos o prompt para interagir com a pessoa permitindo inserir um valor e armazenando em uma variável;

---
+++ desafio1.js
@@ -0,0 +1,19 @@
+alert('Boas vindas ao nosso site');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let SaldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemErro = 'Erro! Preencha todos os campos';
+alert(mensagemErro);
+
+let nome = prompt('Digite o seu nome');
+
+let idade = prompt('Digite a sua idade');
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
